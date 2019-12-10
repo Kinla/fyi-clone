44e30f29a4960c7f739e88a0e67c90a7c469f896
Kinla <sharon.s.c.chien@gmail.com>
2019-12-10 13:47:58 -0500

    init sanity studio and added the categories and authors. also styped the authors UI

---
+++ studio/schemas/author.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default {
+  name: 'author',
+  title: 'Author',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string'
+    },
+    {
+      name: 'domainName',
+      title: 'Domain Name',
+      type: 'string',
+    },
+    {
+      name: 'myColor',
+      type: 'color',
+      title: 'Choose color'
+    },
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      domain: 'domainName',
+      color: 'myColor',
+    },
+    prepare ({ title, domain }) {
+      return {
+        title,
+        subtitle: domain,
+        media: <img style={{width: '16px', height: '16px'}} src={`https://www.google.com/s2/favicons?domain=${domain}`} alt={title} />
+      }
+    }
+  }
+}
---
+++ studio/schemas/siteSettings.js
@@ -0,0 +1,23 @@
+export default {
+  name: 'siteSettings',
+  title: 'Site Settings',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string'
+    },
+    {
+      name: 'metaTitle',
+      title: 'Meta Title',
+      type: 'string'
+    },
+    {
+      name: 'metaDescription',
+      title: 'Meta Description',
+      type: 'text'
+    },
+    ]
+}
+
